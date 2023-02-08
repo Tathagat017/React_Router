7772f6c160b76a30947cb4fea8b117cbdf856c11
Tathagat017 <114250830+Tathagat017@users.noreply.github.com>
2023-02-08 16:12:32 +0530

    with await

--- React_Router/react-router/src/components/SingleUserPage.jsx
+++ React_Router/react-router/src/components/SingleUserPage.jsx
@@ -1,52 +1,42 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 export default function SingleUser() {
-  const { userId } = useParams();
-  const [loading, setLoading] = useState(false);
+  const [loading, loadingState] = useState(false);
   const [user, setUser] = useState([]);
   const [error, setError] = useState(false);
-
-  async function getData(userID) {
-    let url = `https://reqres.in/api/users/${userId}`;
-    fetch(url)
-      .then((res) => {
-        setLoading(true);
-        return res.json();
-      })
-      .then(async (receivedData) => {
-        console.log(receivedData["data"]);
-        setUser(receivedData["data"]);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setError(err);
-      });
-  }
-  // "data": {
-  // "id": 1,
-  // "email": "george.bluth@reqres.in",
-  // "first_name": "George",
-  // "last_name": "Bluth",
-  // "avatar": "https://reqres.in/img/faces/1-image.jpg"
-  // },
-
-  function userShow(user) {
-    return (
-      <div>
-        <h3>{`${user.first_name} ${user.last_name}`}</h3>
-        <img src={user.avatar} alt=""></img>
-        <p>{user.email}</p>
-      </div>
-    );
-  }
-
+  const { userId } = useParams();
+  let value = userId;
+  const getUsers = async (value) => {
+    try {
+      setError(false);
+      loadingState(true);
+      const response = await fetch(`https://reqres.in/api/users/${userId}`);
+      const data = await response.json();
+      console.log(data);
+      let u = [];
+      u.push(data["data"]);
+      setUser(u);
+      loadingState(false);
+    } catch (error) {
+      setError(true);
+      loadingState(false);
+    }
+  };
   useEffect(() => {
-    getData(userId);
+    getUsers();
   }, []);
 
-  //   useEffect(() => {
-  //     getData(userId);
-  //   }, [user]);
-  return loading ? <h1>Loading</h1> : <div>{userShow(user)}</div>;
+  return loading ? (
+    <h1>Loading</h1>
+  ) : (
+    <div>
+      {user.map((user) => (
+        <div key={user.id}>
+          <h1>{user.first_name}</h1>
+          <h2>{user.email}</h2>
+          <img src={user.avatar} alt="" />
+        </div>
+      ))}
+    </div>
+  );
 }
